5951e8407070f3cff5e142d6bb504d59f8f413be
Lucas <lucasrandisi@gmail.com>
2024-10-12 19:28:00 -0300

    Refactor login and register unit testing classes into AuthServiceTest class

--- api/src/test/java/market/api/unit/services/AuthServiceTest.java
+++ api/src/test/java/market/api/unit/services/AuthServiceTest.java
@@ -1,6 +1,10 @@
-package market.api.unit.auth;
+package market.api.unit.services;
 
 import market.api.dtos.auth.LoginRequestDTO;
+import market.api.dtos.auth.RegisterRequestDTO;
+import market.api.exceptions.ConflictException;
+import market.api.models.User;
+import market.api.repositories.UserRepository;
 import market.api.services.AuthService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,28 +16,47 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class LoginTest {
+public class AuthServiceTest {
 	@Mock
 	AuthenticationManager authenticationManager;
+
+	@Mock
+	private UserRepository userRepository;
 
 	@InjectMocks
 	AuthService authService;
 
 	@Test
-	public void throwsExceptionWhenWrongCredentials() {
+	public void login_throwsExceptionWhenUsingWrongCredentials() {
+		when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenThrow(new AuthenticationException("Authentication failed") {});
+
 		LoginRequestDTO loginRequestDTO = new LoginRequestDTO();
 		loginRequestDTO.setEmail("email@test.com");
 		loginRequestDTO.setPassword("password");
 
-		when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenThrow(new AuthenticationException("Authentication failed") {});
-
 		BadCredentialsException exception = assertThrows(BadCredentialsException.class, () -> authService.login(loginRequestDTO));
 		assertEquals(exception.getMessage(), "Incorrect email or password");
+	}
+
+	@Test
+	void register_throwsConflictExceptionOnAlreadyExistingEmail() {
+		RegisterRequestDTO registerRequestDTO= new RegisterRequestDTO();
+		registerRequestDTO.setEmail("email@email.com");
+		registerRequestDTO.setPassword("password");
+		registerRequestDTO.setFirstName("firstName");
+		registerRequestDTO.setLastName("lastName");
+
+		when(userRepository.findByEmail(registerRequestDTO.getEmail())).thenReturn(Optional.of(new User()));
+
+		ConflictException exception = assertThrows(ConflictException.class, () -> authService.register(registerRequestDTO));
+		assertEquals("Email already exists", exception.getMessage());
 	}
 }
