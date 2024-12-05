41f8e4898ff74fe6885df90bcd8e0aa3e58fdc3e
Lucas <lucasrandisi@gmail.com>
2024-12-05 10:45:22 -0300

    Role authorization

--- api/src/main/java/market/api/models/User.java
+++ api/src/main/java/market/api/models/User.java
@@ -4,13 +4,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "users")
@@ -46,7 +47,9 @@ public class User implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return List.of();
+		return roles.stream()
+			.map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName().toUpperCase()))
+			.collect(Collectors.toCollection(HashSet::new));
 	}
 
 	@Override
